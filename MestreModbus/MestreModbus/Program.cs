f35f19c681560d367e9cd659e589d9c67e2a4512
cuki2 <mauricio@krebs.com.br>
2017-10-25 16:15:38 -0200

    Signed-off-by: cuki2 <mauricio@krebs.com.br>

---
+++ MestreModbus/MestreModbus/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modbus.Device;
+using System.IO.Ports;
+
+namespace MestreModbus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort passo = 50;
+
+            SerialPort p = new SerialPort
+            {
+                PortName = "COM2",
+                BaudRate = 19200,
+                DataBits = 8,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                ReadTimeout = 200
+            };
+
+            ModbusSerialMaster m = ModbusSerialMaster.CreateRtu(p);
+
+            try
+            {
+                ushort aux;
+
+                aux = 0;
+                p.Open();
+
+                for (ushort cont = 0; cont < 150; ++cont)
+                {                    
+                    foreach (ushort reg in m.ReadHoldingRegisters(1, (ushort)(cont * passo), passo))
+                    {
+                        Console.Out.WriteLine(aux + " " + reg);
+                        aux++;
+                    }    
+
+                    Console.WriteLine();
+                }
+
+                Console.Out.WriteLine("Leitura feita");
+            }
+            catch (Exception e)
+            {
+                Console.Out.Write(e.StackTrace);
+                Console.Out.WriteLine("\nErro na leitura");
+            }
+            finally
+            {
+                p.Close();
+                Console.Out.WriteLine("Porta fechada");
+            }
+
+            Console.In.ReadLine();
+        }
+    }
+}
--- ModbusServer/ModbusServer/Program.cs
+++ ModbusServer/ModbusServer/Program.cs
@@ -11,13 +11,11 @@ namespace ModbusServer
 {
     class Program
     {
-        public static void CallToChildThread()
+        static void Main(string[] args)
         {
             ModbusSerialSlave slv;
             SerialPort porta;
-            byte m_addr;
 
-            m_addr = 1;
             porta = new SerialPort();
             porta.PortName = "COM1";
             porta.DataBits = 8;
@@ -25,95 +23,18 @@ namespace ModbusServer
             porta.StopBits = StopBits.One;
             porta.BaudRate = 19200;
 
-            slv = ModbusSerialSlave.CreateRtu(m_addr, porta);
-
             try
             {
                 porta.Open();
-                slv.Listen();
             }
-            catch (Exception)
+            catch (Exception e)
             {
                 porta.Close();
-                throw;
-            }
-        }
-
-        public static void CallToChildThread1()
-        {
-            ModbusSerialSlave slv;
-            SerialPort porta;
-            byte m_addr;
-
-            m_addr = 1;
-            porta = new SerialPort();
-            porta.PortName = "COM2";
-            porta.DataBits = 8;
-            porta.Parity = Parity.None;
-            porta.StopBits = StopBits.One;
-            porta.BaudRate = 19200;
-
-            slv = ModbusSerialSlave.CreateRtu(m_addr, porta);
-
-            try
-            {
-                porta.Open();
-                slv.Listen();
-            }
-            catch (Exception)
-            {
-                porta.Close();
-                throw;
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            ThreadStart childref = new ThreadStart(CallToChildThread);
-            Thread childThread = new Thread(childref);
-            childThread.Start();
-
-            ThreadStart childref1 = new ThreadStart(CallToChildThread1);
-            Thread childThread1 = new Thread(childref1);
-            childThread1.Start();
-
-            Console.ReadKey();
-            //for (; ; )
-            //{
-            //    try
-            //    {
-            //        childThread.Start();
-            //    }
-            //    catch (Exception)
-            //    {
-            //        childThread.Abort();
-            //    }
-
-            //    try
-            //    {
-            //        childThread1.Start();
-            //    }
-            //    catch (Exception)
-            //    {
-            //        childThread1.Abort();
-            //    }
-            //}
-        }
-
-        static byte AskPort()
-        {
-            byte ret = 0xFF;
-
-            Console.Out.WriteLine("Escolha uma porta");
-
-            foreach (String str in SerialPort.GetPortNames())
-            {
-                Console.Out.WriteLine(str);
+                Console.Out.WriteLine("erro porta");
+                Console.Out.Write(e.ToString());
             }
 
-            ret = (byte)Console.In.Read();
-
-            return ret;
+            slv = ModbusSerialSlave.CreateRtu(1, porta);
         }
     }
 }
